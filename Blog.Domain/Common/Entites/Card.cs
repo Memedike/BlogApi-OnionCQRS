a533c63b68a2d118ad510b1db05bc4818ee25a5c
mehmet ali ike <mehmet.ike733@gmail.com>
2024-03-01 23:21:26 +0300

    kategori

---
+++ Blog.Domain/Common/Entites/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Domain.Common.Entites
+{
+    public class Card : EntityBase
+    {
+        public Card()
+        {
+
+        }
+        public Card(string Name)
+        {
+            this.Name = Name;
+        }
+        public required string Name { get; set; }
+
+    }
+}
